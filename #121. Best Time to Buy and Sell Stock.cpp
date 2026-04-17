e71840c695d58133f1a4bda8a1df88d7d53c3c4f
Shubh Gupta <shubh210gupta@gmail.com>
2026-04-17 11:17:17 +0530

    Implement maxProfit function for stock prices

---
+++ #121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int mini=prices[0];
+        for(int i=1;i<prices.size();i++){
+                maxi=max(maxi,prices[i]-mini);
+                mini=min(mini,prices[i]);
+        }
+        return maxi;
+    }
+};
